6226fec99413c1bf2fec580a1c6793a879c8f2d1
Govind Yadav <govindyadav@Govinds-MacBook-Pro.local>
2020-11-18 12:00:13 +0530

    Removing unused imports

--- basepair/infra/webapp/analysis.py
+++ basepair/infra/webapp/analysis.py
@@ -1,14 +1,7 @@
 '''Analysis webapp api wrapper'''
 
-# General imports
-import json
-
-# Lib imports
-import requests
-
 # App imports
 from .abstract import Abstract
-from basepair.helpers import eprint
 
 class Analysis(Abstract):
   '''Webapp Analysis class'''
