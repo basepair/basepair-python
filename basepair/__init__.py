1886670643b3eee02e7b18ef1fe92a01251ffccd
blasvicco@gmail.com <blasvicco@gmail.com>
2021-07-23 17:38:39 +0200

    Bumping version.

--- basepair/__init__.py
+++ basepair/__init__.py
@@ -8,7 +8,7 @@ from .utils import colors
 # from .api import basepair
 
 __title__ = 'basepair'
-__version__ = '1.7.3'
+__version__ = '1.7.4'
 __copyright__ = 'Copyright [2017] - [2021] Basepair INC'
 
 
