378e92a32f98c5fb374cf97bcb3175f85034a4e1
Siddhant Kumar Singh <siddhant@basepairtech.com>
2022-02-08 19:19:35 +0530

    BugFix:adding Error message in missing : in create analysis params

--- bin/datatypes/analysis.py
+++ bin/datatypes/analysis.py
@@ -20,10 +20,13 @@ class Analysis:
 
     if args.params:
       for param in args.params:
-        node_id, arg, val = param.split(':')
-        if node_id not in params['node']:
-          params['node'][node_id] = {}
-        params['node'][node_id][arg] = val
+        try:
+          node_id, arg, val = param.split(':')
+          if node_id not in params['node']:
+            params['node'][node_id] = {}
+          params['node'][node_id][arg] = val
+        except:
+          sys.exit('ERROR: Missing : in params values.')
     else:
       eprint('You specified no parameters, submitting with default ones.')
     return bp_api.create_analysis(
