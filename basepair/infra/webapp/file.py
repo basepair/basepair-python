35fccb8ffcb48e2c8598a82e4e56dcf1ee9e413c
Amiay Narayan <35066387+amiaynara@users.noreply.github.com>
2023-09-05 15:17:08 +0530

    Improve: Allow download using CLI with presigned url

--- basepair/infra/webapp/file.py
+++ basepair/infra/webapp/file.py
@@ -1,5 +1,9 @@
 '''File webapp api wrapper'''
 
+# General imports
+import requests
+import json
+
 # App imports
 from .abstract import Abstract
 
@@ -8,3 +12,22 @@ class File(Abstract):
   def __init__(self, cfg):
     super(File, self).__init__(cfg)
     self.endpoint += 'files/'
+
+  def get_presigned_url(self, key):
+    '''Method to call the http request to sign the url'''
+    payload = {'key': key}
+    return self._file_post_request('get_presigned_url', payload)
+
+  def _file_post_request(self, route, payload):
+    '''Call the appropriate endpoint'''
+    try:
+      response = requests.post(
+        f'{self.endpoint}{route}',
+        data=json.dumps(payload),
+        params=self.payload,
+        verify=True,
+        headers={'content-type': 'application/json'}
+      )
+      return self._parse_response(response)
+    except Exception as error:
+      return {'error': True, 'msg': error}
