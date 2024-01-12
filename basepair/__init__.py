5f723dab7c59f75b1793e8ac240f4a93e04ffe53
Amiay Narayan <35066387+amiaynara@users.noreply.github.com>
2024-01-12 14:27:25 +0530

    Update the version to 2.0.12b

--- basepair/__init__.py
+++ basepair/__init__.py
@@ -12,7 +12,7 @@ from .infra.webapp import Analysis, File, Gene, Genome, GenomeFile, Host, Module
 # Exposing the storage wrapper
 
 __title__ = 'basepair'
-__version__ = '2.0.12a'
+__version__ = '2.0.12b'
 __copyright__ = 'Copyright [2017] - [2022] Basepair INC'
 
 
