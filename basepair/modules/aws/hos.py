54046349e3d53d48bf4e790f4fcfa4d713393697
Govind <56910277+govind-basepair@users.noreply.github.com>
2024-03-07 13:22:54 -0500

    Adding api for reference gneome import

--- basepair/modules/aws/hos.py
+++ basepair/modules/aws/hos.py
@@ -20,6 +20,7 @@ class HOS(Service):
       'service_name': 'omics',
     })
     self.sequence_store_id = cfg.get('sequence_store_id')
+    self.reference_store_id = cfg.get('reference_store_id')
     self.role_arn = cfg.get('role_arn')
 
   def get_export_job(self, job_id):
@@ -48,6 +49,21 @@ class HOS(Service):
       self.get_log_msg({
         'exception': error,
         'msg': f'Not able to get HealthOmics import job information: {str(error)}.',
+      })
+      raise error
+    return response
+
+  def get_reference_import_job(self, job_id):
+    '''Get reference import job'''
+    try:
+      response = self.client.get_reference_import_job(
+        id=job_id,
+        referenceStoreId=self.reference_store_id,
+      )
+    except ClientError as error:
+      self.get_log_msg({
+        'exception': error,
+        'msg': f'Not able to get HealthOmics reference import job information: {str(error)}.',
       })
       raise error
     return response
@@ -89,14 +105,33 @@ class HOS(Service):
   def start_import_job(self, sources):
     '''Start import job'''
     try:
+      client_token = str(uuid.uuid4())
       return self.client.start_read_set_import_job(
         sequenceStoreId=self.sequence_store_id,
         roleArn=self.role_arn,
+        clientToken=client_token,
         sources=sources
       )
     except ClientError as error:
       self.get_log_msg({
         'exception': str(error),
         'msg': f'Not able to start HealthOmics import job: {str(error)}.',
+      })
+      raise error
+
+  def start_reference_import_job(self, sources):
+    '''Start reference import job'''
+    try:
+      client_token = str(uuid.uuid4())
+      return self.client.start_reference_import_job(
+        referenceStoreId=self.reference_store_id,
+        roleArn=self.role_arn,
+        clientToken=client_token,
+        sources=sources
+      )
+    except ClientError as error:
+      self.get_log_msg({
+        'exception': str(error),
+        'msg': f'Not able to start HealthOmics reference import job: {str(error)}.',
       })
       raise error
