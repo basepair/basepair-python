455534a83b7be484f3bcb5238d828eedf6a8827d
Amiay Narayan <amiayn@iitbhilai.ac.in>
2024-04-30 15:49:10 +0530

    Create version 2.1.1

--- basepair/__init__.py
+++ basepair/__init__.py
@@ -12,7 +12,7 @@ from .infra.webapp import Analysis, File, Gene, Genome, GenomeFile, Host, Module
 # Exposing the storage wrapper
 
 __title__ = 'basepair'
-__version__ = '2.1.0'
+__version__ = '2.1.1'
 __copyright__ = 'Copyright [2017] - [2022] Basepair INC'
 
 
