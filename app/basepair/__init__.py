952626ed2d0d0c9f185e9edc65a0ddf6da00c65c
Blas Vicco <blasvicco@gmail.com>
2025-03-07 12:32:04 +0100

    Set proper version for production release.

--- app/basepair/__init__.py
+++ app/basepair/__init__.py
@@ -12,7 +12,7 @@ from .infra.webapp import Analysis, File, Gene, Genome, GenomeFile, Host, Module
 # Exposing the storage wrapper
 
 __title__ = 'basepair'
-__version__ = '2.2.7beta'
+__version__ = '2.2.7'
 __copyright__ = 'Copyright [2017] - [2024] Basepair INC'
 
 
