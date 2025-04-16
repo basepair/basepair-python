bbbe96022932d6be281ccbb100b21e9afa0c38ff
Amiay Narayan <amiayn@iitbhilai.ac.in>
2025-04-16 17:20:47 +0530

    Improvement: Only check for the latest version if it is not onprem version of the package

--- app/basepair/__init__.py
+++ app/basepair/__init__.py
@@ -12,22 +12,24 @@ from .infra.webapp import Analysis, File, Gene, Genome, GenomeFile, Host, Module
 # Exposing the storage wrapper
 
 __title__ = 'basepair'
-__version__ = '2.2.9_onprem'
+__version__ = '2.2.9+onprem'
 __copyright__ = 'Copyright [2017] - [2024] Basepair INC'
 
 
 JSON_URL = 'https://pypi.python.org/pypi/{}/json'.format(__title__)
-try:
-    resp = requests.get(JSON_URL, timeout=1)
-    if resp.status_code == 200:
-        latest_version = resp.json()['info']['version']
-        if latest_version != __version__:
-            print(colors.color.warning(
-                'WARNING: The latest version of basepair package is {}. '
-                'Please upgrade to avail the latest features and bug-fixes'
-                ''.format(latest_version)), file=sys.stderr)
-except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
-    print('warning: no internet', file=sys.stderr)
+
+if 'onprem' not in __version__:
+    try:
+        resp = requests.get(JSON_URL, timeout=1)
+        if resp.status_code == 200:
+            latest_version = resp.json()['info']['version']
+            if latest_version != __version__:
+                print(colors.color.warning(
+                    'WARNING: The latest version of basepair package is {}. '
+                    'Please upgrade to avail the latest features and bug-fixes'
+                    ''.format(latest_version)), file=sys.stderr)
+    except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
+        print('warning: no internet', file=sys.stderr)
 
 
 def connect(*args, **kwargs):
